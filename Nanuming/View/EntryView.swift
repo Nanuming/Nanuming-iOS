bf09c9f1b5040322eaab8403826ec74b4a37e4c9
slr-09 <ge12090@naver.com>
2024-01-28 01:44:10 +0900

    NNM-66 엔트리 화면 구현

---
+++ Nanuming/View/EntryView.swift
@@ -0,0 +1,36 @@
+//
+//  EntryView.swift
+//  Nanuming
+//
+//  Created by 가은 on 1/28/24.
+//
+
+import SwiftUI
+
+var screenWidth = UIScreen.main.bounds.size.width
+var screenHeight = UIScreen.main.bounds.size.height
+
+struct EntryView: View {
+    var body: some View {
+        VStack {
+            Image("Logo")
+                .frame(width: screenWidth*0.8,height: screenWidth*0.8)
+            RoundedRectangle(cornerRadius: 5)
+                .frame(width: screenWidth*0.85, height: 50)
+                .foregroundColor(.white)
+                .shadow(color: .black.opacity(0.25), radius: 5, x: 0, y: 4)
+                .overlay(
+                    HStack {
+                        Image("google_logo")
+                            .frame(width: 30, height: 30)
+                        Text("Sign in with Google")
+                            .font(.system(size: 17, weight: .semibold))
+                    }
+                )
+        }
+    }
+}
+
+#Preview {
+    EntryView()
+}
