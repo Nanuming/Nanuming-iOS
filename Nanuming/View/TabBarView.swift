6c5fdd331a50967eca17bcb5fba39a52b48cce8d
slr-09 <ge12090@naver.com>
2024-01-27 18:15:58 +0900

    NNM-70 탭 바 기능 구현

---
+++ Nanuming/View/TabBarView.swift
@@ -0,0 +1,32 @@
+//
+//  TabBarView.swift
+//  Nanuming
+//
+//  Created by 가은 on 1/27/24.
+//
+
+import SwiftUI
+
+struct TabBarView: View {
+    var body: some View {
+        VStack {
+            // 탭 뷰
+            TabView {
+                // 홈 탭 (지도&게시물 리스트)
+                MapView()
+                    .tabItem({
+                        Label("홈", systemImage: "house")
+                    })
+                // my 탭
+                MyPageView()
+                    .tabItem({
+                        Label("My", systemImage: "person.circle")
+                    })
+            }
+        }
+    }
+}
+
+#Preview {
+    TabBarView()
+}
