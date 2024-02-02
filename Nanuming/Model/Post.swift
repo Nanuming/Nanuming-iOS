a4005084fc506484894ca3f303afd60d5240123f
slr-09 <ge12090@naver.com>
2024-02-02 21:05:46 +0900

    NNM-87 Post Model 생성

---
+++ Nanuming/Model/Post.swift
@@ -0,0 +1,20 @@
+//
+//  Post.swift
+//  Nanuming
+//
+//  Created by 가은 on 2/2/24.
+//
+
+import SwiftUI
+
+struct Post {
+    var publisher: String = "유가은"               // 게시물 올린 사람의 닉네임
+    var createdDate: String = "2024.01.31"      // 생성일자
+    var title: String = "루피 인형 나눔합니다"
+    var image: [String] = [""]
+    var category: String = "장난감"
+    var location: String = "자양4동 어린이집"
+    var contents: String = "나눔합니다"
+    var likeNum: Int = 0
+    var isMyPost: Bool = false                  // 본인 게시물인지
+}
---
+++ Nanuming/View/Post/PostListCell.swift
@@ -0,0 +1,18 @@
+//
+//  PostListCell.swift
+//  Nanuming
+//
+//  Created by 가은 on 2/2/24.
+//
+
+import SwiftUI
+
+struct PostListCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PostListCell()
+}
