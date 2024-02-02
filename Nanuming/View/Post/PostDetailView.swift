77723102fa4aa85c3e46f2e4572e019c18c6ee73
slr-09 <ge12090@naver.com>
2024-02-03 01:25:52 +0900

    NNM-90 이미지 pageTabViewStyle 구현

--- Nanuming/View/Post/PostDetailView.swift
+++ Nanuming/View/Post/PostDetailView.swift
@@ -8,11 +8,28 @@
 import SwiftUI
 
 struct PostDetailView: View {
+    @Binding var post: Post
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView(showsIndicators: false) {
+            VStack {
+                // 이미지
+                TabView {
+                    // TODO: url을 사용하기 위해서는 변경 작업 필요
+                    ForEach(post.image, id: \.self) { url in
+                        Image(url)
+                            .resizable()
+                            .background(.gray100)
+                    }
+                }
+                .tabViewStyle(PageTabViewStyle())
+                .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .never))
+                .frame(width: screenWidth, height: screenWidth)
+            }
+        }
     }
 }
 
 #Preview {
-    PostDetailView()
+    PostDetailView(post: .constant(Post(publisher: "유가은", createdDate: "2024.01.31", title: "루피 인형 나눔", image: ["Logo", "google_logo"], category: "장난감", location: "자양4동 어린이집", contents: "나눔나눔", isMyPost: false)))
 }
