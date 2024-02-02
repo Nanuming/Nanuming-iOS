84b0b04ec8435db80a2a515fc08045f537f5b862
slr-09 <ge12090@naver.com>
2024-02-03 03:07:56 +0900

    NNM-93 나눔받기 버튼 추가

--- Nanuming/View/Post/PostDetailView.swift
+++ Nanuming/View/Post/PostDetailView.swift
@@ -11,73 +11,86 @@ struct PostDetailView: View {
     @Binding var post: Post
 
     var body: some View {
-        ScrollView(showsIndicators: false) {
-            VStack(spacing: 0) {
-                // 이미지
-                TabView {
-                    // TODO: url을 사용하기 위해서는 변경 작업 필요
-                    ForEach(post.image, id: \.self) { url in
-                        Image(url)
-                            .resizable()
-                            .background(.gray100)
-                    }
-                }
-                .tabViewStyle(PageTabViewStyle())
-                .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .never))
-                .frame(width: screenWidth, height: screenWidth)
-                
-                // 게시 정보
-                HStack(spacing: 5) {
-                    // 게시자
-                    Text(post.publisher)
-                        .font(.system(size: 18, weight: .bold))
-                        .foregroundColor(.textBlack)
-                    Text("님")
-                        .font(.system(size: 16, weight: .medium))
-                    Spacer()
-                    // 생성일자
-                    Text(post.createdDate)
-                        .font(.system(size: 14, weight: .medium))
-                }
-                .foregroundColor(.gray200)
-                .frame(height: 50)
-                .padding(EdgeInsets(top: 5, leading: 20, bottom: 5, trailing: 20))
-                .background(.gray50)
-                
-                // 게시물 상세
-                VStack(alignment: .leading, spacing: 15) {
-                    // 타이틀
-                    Text(post.title)
-                        .font(.system(size: 24, weight: .bold))
-                        .foregroundColor(.textBlack)
-                    HStack(spacing: 10) {
-                        // 카테고리
-                        Text("#"+post.category)
-                            .foregroundColor(.white)
-                            .padding(EdgeInsets(top: 5, leading: 10, bottom: 5, trailing: 10))
-                            .background(.greenSelected)
-                            .cornerRadius(14)
-                        //장소
-                        Button {
-                            // TODO: 지도 연결
-                        } label: {
-                            HStack(spacing: 3) {
-                                Image("post_cell_map")
-                                    .frame(width: 17, height: 17)
-                                Text(post.location)
-                                    .foregroundColor(.gray300)
-                            }
+        ZStack(alignment: .bottom) {
+            ScrollView(showsIndicators: false) {
+                VStack(spacing: 0) {
+                    // 이미지
+                    TabView {
+                        // TODO: url을 사용하기 위해서는 변경 작업 필요
+                        ForEach(post.image, id: \.self) { url in
+                            Image(url)
+                                .resizable()
+                                .background(.gray100)
                         }
                     }
-                    Divider()
-                    // 내용
-                    Text(post.contents)
-                        .foregroundColor(.textBlack)
-                        .lineSpacing(5)
+                    .tabViewStyle(PageTabViewStyle())
+                    .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .never))
+                    .frame(width: screenWidth, height: screenWidth)
+                    
+                    // 게시 정보
+                    HStack(spacing: 5) {
+                        // 게시자
+                        Text(post.publisher)
+                            .font(.system(size: 18, weight: .bold))
+                            .foregroundColor(.textBlack)
+                        Text("님")
+                            .font(.system(size: 16, weight: .medium))
+                        Spacer()
+                        // 생성일자
+                        Text(post.createdDate)
+                            .font(.system(size: 14, weight: .medium))
+                    }
+                    .foregroundColor(.gray200)
+                    .frame(height: 50)
+                    .padding(EdgeInsets(top: 5, leading: 20, bottom: 5, trailing: 20))
+                    .background(.gray50)
+                    
+                    // 게시물 상세
+                    VStack(alignment: .leading, spacing: 15) {
+                        // 타이틀
+                        Text(post.title)
+                            .font(.system(size: 24, weight: .bold))
+                            .foregroundColor(.textBlack)
+                        HStack(spacing: 10) {
+                            // 카테고리
+                            Text("#"+post.category)
+                                .foregroundColor(.white)
+                                .padding(EdgeInsets(top: 5, leading: 10, bottom: 5, trailing: 10))
+                                .background(.greenSelected)
+                                .cornerRadius(14)
+                            //장소
+                            Button {
+                                // TODO: 지도 연결
+                            } label: {
+                                HStack(spacing: 3) {
+                                    Image("post_cell_map")
+                                        .frame(width: 17, height: 17)
+                                    Text(post.location)
+                                        .foregroundColor(.gray300)
+                                }
+                            }
+                        }
+                        Divider()
+                        // 내용
+                        Text(post.contents)
+                            .foregroundColor(.textBlack)
+                            .lineSpacing(5)
+                    }
+                    .padding(EdgeInsets(top: 25, leading: 15, bottom: 25, trailing: 15))
+                    .frame(width: screenWidth)
+                    .font(.system(size: 15, weight: .medium))
                 }
-                .padding(EdgeInsets(top: 25, leading: 15, bottom: 25, trailing: 15))
-                .frame(width: screenWidth)
-                .font(.system(size: 15, weight: .medium))
+            }
+            Button {
+                // 게시물 생성 페이지로 이동
+            } label: {
+                RoundedRectangle(cornerRadius: 10)
+                    .frame(width: screenWidth*0.9, height: 50)
+                    .overlay(
+                        Text("나눔받기")
+                            .font(.system(size: 16, weight: .bold))
+                            .foregroundColor(.white)
+                    )
             }
         }
     }
