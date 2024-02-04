953b517314cff0e10b69f58755b55e52ab160147
slr-09 <ge12090@naver.com>
2024-02-04 21:23:43 +0900

    Feat: Post model optional 변경

---
+++ Nanuming/Utils/PostImagePicker.swift
@@ -0,0 +1,18 @@
+//
+//  PostImagePicker.swift
+//  Nanuming
+//
+//  Created by 가은 on 2/4/24.
+//
+
+import SwiftUI
+
+struct PostImagePicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PostImagePicker()
+}
