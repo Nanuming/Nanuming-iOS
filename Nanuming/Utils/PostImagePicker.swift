b37dc3bff18e8d764e52092e2b3c54cfd45bda82
slr-09 <ge12090@naver.com>
2024-02-05 17:26:52 +0900

    Feat: post image picker 구현 중

--- Nanuming/Utils/PostImagePicker.swift
+++ Nanuming/Utils/PostImagePicker.swift
@@ -5,14 +5,48 @@
 //  Created by 가은 on 2/4/24.
 //
 
+import PhotosUI
 import SwiftUI
 
 struct PostImagePicker: View {
+    @Binding var post: Post
+    @State var selectedPhotos: [PhotosPickerItem] = []
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            PhotosPicker(selection: $selectedPhotos, maxSelectionCount: 10, matching: .images) {
+                RoundedRectangle(cornerRadius: 8)
+                    .stroke(.gray100, lineWidth: 1)
+                    .frame(width: 70, height: 70)
+                    .overlay(
+                        VStack(spacing: 3) {
+                            Image(systemName: "camera.fill")
+                                .frame(width: 25, height: 25)
+                            Text(String(selectedPhotos.count) + "/10")
+                                .font(.system(size: 12, weight: .medium))
+                        }
+                        .foregroundColor(.gray200)
+                    )
+            }
+            .onChange(of: selectedPhotos, initial: false) { _, _ in
+                guard let item = selectedPhotos.first else { return }
+                item.loadTransferable(type: Data.self) { result in
+                    switch result {
+                    case .success(let data):
+                        if let data = data {
+                            print(data)
+                        } else {
+                            print("data is nil")
+                        }
+                    case .failure(let failure):
+                        fatalError("\(failure)")
+                    }
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    PostImagePicker()
+    PostImagePicker(post: .constant(Post()))
 }
--- Nanuming/View/Post/CreatePostView.swift
+++ Nanuming/View/Post/CreatePostView.swift
@@ -12,20 +12,35 @@ struct CreatePostView: View {
     
     var body: some View {
         NavigationView {
-            VStack(alignment: .leading) {
-                Text("제목")
-                    .font(.system(size: 18, weight: .semibold))
-                    .foregroundColor(.textBlack)
-                ZStack(alignment: .bottom) {
-                    TextField("제목", text: $title)
-                        .font(.system(size: 16, weight: .medium))
+            VStack(alignment: .leading, spacing: 30) {
+                // 제목
+                VStack(alignment: .leading) {
+                    Text("제목")
+                        .font(.system(size: 18, weight: .semibold))
                         .foregroundColor(.textBlack)
-                        .frame(height: 30)
-                    Rectangle()
-                        .frame(height: 0.75)
+                    ZStack(alignment: .bottom) {
+                        TextField("제목", text: $title)
+                            .font(.system(size: 16, weight: .medium))
+                            .foregroundColor(.textBlack)
+                            .frame(height: 30)
+                        Rectangle()
+                            .frame(height: 0.75)
+                    }
+                    .foregroundColor(.gray100)
+                    .frame(width: screenWidth*0.85)
                 }
-                .foregroundColor(.gray100)
-                .frame(width: screenWidth*0.85)
+                // 카테고리
+                
+                // 사진
+                VStack {
+                    Text("사진")
+                        .font(.system(size: 18, weight: .semibold))
+                        .foregroundColor(.textBlack)
+                    
+                }
+                // 장소
+                
+                // 설명
             }
         }
     }
