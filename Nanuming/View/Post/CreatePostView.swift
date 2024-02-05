7731604d1cbc611c422a6396e9599da72961e5b8
slr-09 <ge12090@naver.com>
2024-02-05 18:20:38 +0900

    NNM-102 예비 등록하기 버튼 추가

--- Nanuming/View/Post/CreatePostView.swift
+++ Nanuming/View/Post/CreatePostView.swift
@@ -14,67 +14,83 @@ struct CreatePostView: View {
     
     var body: some View {
         NavigationView {
-            VStack(alignment: .leading, spacing: 30) {
-                // 제목
-                VStack(alignment: .leading) {
-                    Text("제목")
-                        .font(.system(size: 18, weight: .semibold))
-                        .foregroundColor(.textBlack)
-                    ZStack(alignment: .bottom) {
-                        TextField("제목", text: $title)
-                            .font(.system(size: 16, weight: .medium))
-                            .foregroundColor(.textBlack)
-                            .frame(height: 30)
-                        Rectangle()
-                            .frame(height: 0.75)
-                    }
-                    .foregroundColor(.gray100)
-                }
-                
-                // 카테고리
-                
-                // 사진
-                VStack(alignment: .leading) {
-                    Text("사진")
-                        .font(.system(size: 18, weight: .semibold))
-                        .foregroundColor(.textBlack)
-                    postImage
-                }
-                
-                // 장소
-                VStack(alignment: .leading) {
-                    Text("장소")
-                        .font(.system(size: 18, weight: .semibold))
-                        .foregroundColor(.textBlack)
-                    Button {
-                        // 장소 검색 화면으로 이동
-                    } label: {
-                        ZStack(alignment: .bottomLeading) {
-                            TextField("장소 검색하기", text: $title)
-                                .font(.system(size: 16, weight: .medium))
-                                .multilineTextAlignment(.leading)
-                                .disabled(true)
-                                .frame(height: 30)
-                            Rectangle()
-                                .frame(height: 0.75)
+            ZStack(alignment: .bottom) {
+                ScrollView(showsIndicators: false) {
+                    VStack(alignment: .leading, spacing: 30) {
+                        // 제목
+                        VStack(alignment: .leading) {
+                            Text("제목")
+                                .font(.system(size: 18, weight: .semibold))
+                                .foregroundColor(.textBlack)
+                            ZStack(alignment: .bottom) {
+                                TextField("제목", text: $title)
+                                    .font(.system(size: 16, weight: .medium))
+                                    .foregroundColor(.textBlack)
+                                    .frame(height: 30)
+                                Rectangle()
+                                    .frame(height: 0.75)
+                            }
+                            .foregroundColor(.gray100)
                         }
-                        .foregroundColor(.gray100)
+                        
+                        // 카테고리
+                        
+                        // 사진
+                        VStack(alignment: .leading) {
+                            Text("사진")
+                                .font(.system(size: 18, weight: .semibold))
+                                .foregroundColor(.textBlack)
+                            postImage
+                        }
+                        
+                        // 장소
+                        VStack(alignment: .leading) {
+                            Text("장소")
+                                .font(.system(size: 18, weight: .semibold))
+                                .foregroundColor(.textBlack)
+                            Button {
+                                // 장소 검색 화면으로 이동
+                            } label: {
+                                ZStack(alignment: .bottomLeading) {
+                                    TextField("장소 검색하기", text: $title)
+                                        .font(.system(size: 16, weight: .medium))
+                                        .multilineTextAlignment(.leading)
+                                        .disabled(true)
+                                        .frame(height: 30)
+                                    Rectangle()
+                                        .frame(height: 0.75)
+                                }
+                                .foregroundColor(.gray100)
+                            }
+                        }
+                        
+                        // 설명
+                        VStack(alignment: .leading) {
+                            Text("설명")
+                                .font(.system(size: 18, weight: .semibold))
+                                .foregroundColor(.textBlack)
+                            RoundedRectangle(cornerRadius: 8)
+                                .strokeBorder(.gray100)
+                                .frame(height: 150)
+                                .overlay(alignment: .top, content: {
+                                    TextField("물품에 대한 자세한 설명을 적어주세요!", text: $contents, axis: .vertical)
+                                        .font(.system(size: 16, weight: .medium))
+                                        .padding()
+                                })
+                        }
                     }
+                    .padding(.bottom, 70)
                 }
-                
-                // 설명
-                VStack(alignment: .leading) {
-                    Text("설명")
-                        .font(.system(size: 18, weight: .semibold))
-                        .foregroundColor(.textBlack)
-                    RoundedRectangle(cornerRadius: 8)
-                        .strokeBorder(.gray100)
-                        .frame(height: 150)
-                        .overlay(alignment: .top, content: {
-                            TextField("물품에 대한 자세한 설명을 적어주세요!", text: $contents, axis: .vertical)
-                                .font(.system(size: 16, weight: .medium))
-                                .padding()
-                        })
+                Button {
+                    // 게시물 예비 등록
+                } label: {
+                    RoundedRectangle(cornerRadius: 10)
+                        .frame(width: screenWidth * 0.9, height: 50)
+                        .overlay(
+                            Text("예비 등록하기")
+                                .font(.system(size: 16, weight: .bold))
+                                .foregroundColor(.white)
+                        )
                 }
             }
             .frame(width: screenWidth*0.85)
