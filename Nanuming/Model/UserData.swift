1ee7ae90a580e3804108f83f5ff14ba0a465e0fd
jikbb2 <jikbb2@gmail.com>
2024-02-07 17:22:33 +0900

    Add UserData model file

---
+++ Nanuming/Model/UserData.swift
@@ -0,0 +1,20 @@
+//
+//  UserData.swift
+//  Nanuming
+//
+//  Created by byeoungjik on 2/7/24.
+//
+
+import Foundation
+
+struct UserData {
+    let url:URL?
+    let name: String
+    let email: String
+    
+    init(url: URL?, name: String, email: String) {
+        self.url = url
+        self.name = name
+        self.email = email
+    }
+}
