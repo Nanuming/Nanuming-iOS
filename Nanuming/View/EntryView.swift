0fbd17c031ef398165f9fb25bca684ba372c542d
jikbb2 <jikbb2@gmail.com>
2024-02-08 14:12:43 +0900

    NNM-112 GIDToken 추출

--- Nanuming/View/EntryView.swift
+++ Nanuming/View/EntryView.swift
@@ -34,7 +34,7 @@ struct EntryView: View {
                         action: {
                             googleLogin()
                         })
-                    .frame(width: 300, height: 60, alignment: .center)
+                    .frame(width: screenWidth*0.85, height: 50, alignment: .center)
                 }
                 .navigationDestination(isPresented: $isLogined, destination: {
                     TabBarView(userData: $userData)
@@ -62,7 +62,11 @@ struct EntryView: View {
             checkState()
         })
         .alert(LocalizedStringKey("Failed Login"), isPresented: $isAlert) {
-            Text("ok.")
+            Button(action: {
+                isAlert = false
+            }, label: {
+                Text("OK")
+            })
         } message: {
             Text("please try again.")
         }
@@ -81,25 +85,75 @@ struct EntryView: View {
         }
     }
     func googleLogin() {
-        guard let presentingViewController = (UIApplication.shared.connectedScenes.first as? UIWindowScene)?.windows.first?.rootViewController else { return }
-        GIDSignIn.sharedInstance.signIn(withPresenting: presentingViewController) {
-            signInResult, error in guard let result = signInResult else {
-                isAlert = true
+        guard let presentingViewController = (UIApplication.shared.connectedScenes.first as? UIWindowScene)?.windows.first?.rootViewController.self else { return }
+        print("presentingViewController: \(presentingViewController)")
+//        GIDSignIn.sharedInstance.signIn(withPresenting: presentingViewController) {
+//            signInResult, error in guard let result = signInResult else {
+//                if let signInError = error {
+//                    print("Login error: \(signInError.localizedDescription)")
+//                    isAlert = true
+//                }
+//                return
+//            }
+//            guard let profile = result.user.profile else { return }
+//            let data = UserData(url: profile.imageURL(withDimension: 180), name: profile.name, email: profile.email)
+//            userData = data
+//            isLogined = true
+//            print("userData: \(userData)")
+//            
+//        }
+        GIDSignIn.sharedInstance.signIn(withPresenting: presentingViewController) { signInResult, error in
+            guard error == nil else {
+                print("error: \(String(describing: error?.localizedDescription))")
                 return
             }
-            guard let profile = result.user.profile else { return }
-            let data = UserData(url: profile.imageURL(withDimension: 180), name: profile.name, email: profile.email)
-            userData = data
-            isLogined = true
+            guard let signInResult = signInResult else { return }
+
+            signInResult.user.refreshTokensIfNeeded { user, error in
+                guard error == nil else {
+                    print("error")
+                    return
+                }
+                guard let user = user else { return }
+
+                let idToken = user.idToken
+                print("idToken\(String(describing: idToken))")
+                // Send ID token to backen
+                tokenSignIn(idToken: "google_12345")
+            }
+        }
+        func tokenSignIn(idToken: String) {
+            guard let authData = try? JSONEncoder().encode(["idToken": idToken]) else {
+                return
+            }
+            let url = URL(string: "/api/auth/login")!
+            var request = URLRequest(url: url)
+            request.httpMethod = "POST"
+            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+
+            let task = URLSession.shared.uploadTask(with: request, from: authData) { data, response, error in
+                // Handle response from your backend.
+                isLogined = true
+            }
+            task.resume()
+        }
+        struct userInfo {
+            var success: Bool? = false
+            var status: Int? = 0
+            var message: String? = nil
+            var data: UserData? = nil
             
         }
+        struct UserData {
+            var providerId: String? = nil
+            var nickname: String? = nil
+            var token: Token? = nil
+        }
+        struct Token {
+            var accessToken: String? = nil
+            var refreshToken: String? = nil
+        }
     }
-
-//      func getRootViewController() -> UIViewController {
-//        // Function to get the root view controller of the app
-//        return UIApplication.shared.windows.first!.rootViewController!
-//      }
-    
 }
 //private struct ViewControllerRepresentable: UIViewControllerRepresentable {
 //  let viewController = UIViewController()
