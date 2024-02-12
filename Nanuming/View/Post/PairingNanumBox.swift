8a15ee879673a08baaf15b99b23260bb4cbeca76
jikbb2 <jikbb2@gmail.com>
2024-02-12 22:36:48 +0900

    NNM-133 고유번호 TextField 구현

--- Nanuming/View/Post/PairingNanumBox.swift
+++ Nanuming/View/Post/PairingNanumBox.swift
@@ -8,8 +8,27 @@
 import SwiftUI
 
 struct PairingNanumBox: View {
+    @State private var identifyingNumber: String = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                Text("고유 번호를 입력해주세요.")
+                    .font(.system(size: 24, weight: .semibold))
+                    .foregroundColor(.textBlack)
+                RoundedRectangle(cornerRadius: 10)
+                    .stroke(.gray300, lineWidth: 2.0)
+                    .frame(width: screenWidth * 0.85, height: 43)
+                    .overlay(alignment: .center, content: {
+                        TextField(text: $identifyingNumber, label: {
+                            Text("나누밍 상자 고유번호 입력 ")
+                        })
+                        .padding()
+                    })
+                    .padding()
+                
+            }
+            
+        }
     }
 }
 
