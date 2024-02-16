4adda7bd55e68f00d5cd7eee471ebf30923d4d34
slr-09 <ge12090@naver.com>
2024-02-16 18:53:33 +0900

    Style: mypage button view

--- Nanuming/View/MyPageView.swift
+++ Nanuming/View/MyPageView.swift
@@ -24,7 +24,35 @@ struct MyPageView: View {
             Rectangle()
                 .frame(width: screenWidth, height: 15)
                 .foregroundColor(.gray50)
+            
+            VStack(alignment: .leading, spacing: 20) {
+                buttonBuilder(imageName: "heart", text: "나의 나눔")
+                Divider()
+                buttonBuilder(imageName: "info.circle", text: "버전 정보")
+                Divider()
+                buttonBuilder(imageName: "book.pages", text: "이용약관")
+                Divider()
+                buttonBuilder(imageName: "globe", text: "언어 설정")
+            }
+            .padding(EdgeInsets(top: 50, leading: 30, bottom: 0, trailing: 30))
             Spacer()
+        }
+        
+    }
+    
+    @ViewBuilder
+    func buttonBuilder(imageName: String, text: String) -> some View {
+        Button {
+            
+        } label: {
+            HStack(spacing: 15) {
+                Image(systemName: imageName)
+                    .resizable()
+                    .frame(width: 20, height: 20)
+                Text(text)
+                    .font(.system(size: 16, weight: .medium))
+            }
+            .foregroundColor(.textBlack)
         }
     }
 }
