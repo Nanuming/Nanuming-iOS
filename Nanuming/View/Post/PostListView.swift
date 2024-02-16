4af843f48a3d2b4da199898d8ce88ab4315aaf84
slr-09 <ge12090@naver.com>
2024-02-16 19:45:30 +0900

    Style: 특정 거점 물품 리스트 뷰

--- Nanuming/View/Post/PostListView.swift
+++ Nanuming/View/Post/PostListView.swift
@@ -8,11 +8,42 @@
 import SwiftUI
 
 struct PostListView: View {
+    @State var postList: [PostCellByLocation]
+    @State private var isPresentedLocationPostList = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ScrollView {
+                VStack {
+                    ForEach(postList, id: \.itemId) { postcell in
+
+                        let post = Post(title: postcell.title, category: postcell.categoryName, location: postcell.locationName)
+
+                        // modal로 띄우기
+                        Button {
+                            isPresentedLocationPostList = true
+                        } label: {
+                            PostListCell(post: .constant(post))
+                        }
+                        .fullScreenCover(isPresented: $isPresentedLocationPostList) {
+                            PostDetailView(itemId: postcell.itemId)
+                        }
+                    }
+                }
+            }
+            .navigationBarTitle("\(postList[0].locationName)에 있는 물품", displayMode: .inline)
+            .navigationBarItems(
+                leading: Button(action: {
+                    
+                }) {
+                    Image(systemName: "chevron.backward")
+                        .foregroundColor(.textBlack)
+                        .frame(width: 30, height: 30)
+                }
+            )
+        }
     }
 }
 
 #Preview {
-    PostListView()
+    PostListView(postList: [PostCellByLocation(itemId: 1, mainItemImageUrl: "", title: "gh", locationName: "광진 어린이집", categoryName: "cate"), PostCellByLocation(itemId: 1, mainItemImageUrl: "", title: "gh", locationName: "sadfs", categoryName: "cate")])
 }
