c8e05e059a0046afd9dfd875d1c91407d8da27d1
slr-09 <ge12090@naver.com>
2024-02-17 22:35:56 +0900

    Feat: my post custom tab view

--- Nanuming/View/Mypage/MyPostView.swift
+++ Nanuming/View/Mypage/MyPostView.swift
@@ -7,9 +7,34 @@
 
 import SwiftUI
 
+enum myPostTapInfo: String, CaseIterable {
+    case beforeCreate = "등록 대기"
+    case waiting = "나눔 중"
+    case reserving = "예약 중"
+    case complete = "나눔 완료"
+}
+
 struct MyPostView: View {
+    @State private var selectedTab: Int = 0
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Picker("Flavor", selection: $selectedTab) {
+            ForEach(myPostTapInfo.allCases.indices, id: \.self) { index in
+                Text(myPostTapInfo.allCases[index].rawValue).tag(index)
+            }
+        }
+        .pickerStyle(.segmented)
+        .padding()
+        switch selectedTab {
+            case 0:
+                Text("첫 번째 탭의 내용")
+            case 1:
+                Text("2 번째 탭의 내용")
+            case 2:
+                Text("3 번째 탭의 내용")
+            default:
+                Text("4 번째 탭의 내용")
+        }
+        Spacer()
     }
 }
 
