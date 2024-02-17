efeb768cb2eaebcc18e0fc8133a78b225c807781
slr-09 <ge12090@naver.com>
2024-02-17 21:29:41 +0900

    Feat: 나의 나눔 클릭 시 MyPostView 연결

---
+++ Nanuming/View/Mypage/MyPostView.swift
@@ -0,0 +1,18 @@
+//
+//  MyPostView.swift
+//  Nanuming
+//
+//  Created by 가은 on 2/17/24.
+//
+
+import SwiftUI
+
+struct MyPostView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyPostView()
+}
